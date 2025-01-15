349bd87a8809dc140714a8b7a0ceba6640f3f9ed
Ha-limLee <leehalim09@gmail.com>
2025-01-15 23:28:32 +0900

    test: keep contextLoads test from initializing DB

--- workspaces/backend/src/test/java/plainkanban/backend/BackendApplicationTests.java
+++ workspaces/backend/src/test/java/plainkanban/backend/BackendApplicationTests.java
@@ -1,9 +1,7 @@
 package plainkanban.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BackendApplicationTests {
 
 	@Test
